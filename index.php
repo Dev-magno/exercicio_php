5a8863f5a0b8fa66a4888e036b6d0ca8a18fbef4
Dev-magno <magnolopes1212@gmail.com>
2024-07-11 17:30:54 -0300

    fiz mais algumas questões da atividade

--- index.php
+++ index.php
@@ -47,16 +47,88 @@ echo retornarArgumento();
 
 // Q7
 function verificarSenha($senha){
-    if(ctype_upper($senha) && ctype_lower($senha) && ctype_digit($senha) && strlen($senha)>=8){
+    // variaveis para verificar se false ou true
+    $temMaiscula = false;
+    $temMinuscula = false;
+    $temDigito = false;
+    $temOito = false;
+
+    // Verifica o comprimento da senha
+    if(strlen($senha) >= 8){
+        $temOito = true;
+    }
     
-        echo 'Válida';
+    // faz uma iteração no array e verificar cada caractere da senha
+    foreach(str_split($senha) as $s){
+         if(ctype_upper($s)){
+            $temMaiscula = true;
+        }
+        elseif(ctype_lower($s)){
+            $temMinuscula = true;
+        }
+        elseif(ctype_digit($s)){
+            $temDigito = true;
+        }
     }
 
-    else{
-       
-        echo 'Inválida';
-    }
+      // verifica se a senha atende a todos os requisitos
+        if($temMaiscula && $temMinuscula && $temDigito && $temOito){
+            echo "Senha válida!";
+        }
+        else{
+            echo "Senha inválida!";
+        }
+}
+verificarSenha("1Mnut674");
 
+//Q8
+$idade = 18;
+echo  ($idade >= 18)? "Maior" : "Menor";
+
+// Q9
+$alunos = array(
+    'Junior' => 9.5,
+    'Maria' => 10,
+    'Paulo' => 6,
+    'Ana' => 8.5,
+    'Pedro' => 5.5,
+    'Julia' => 6.5
+);
+
+foreach($alunos as $nome => $nota){
+    if($nota >= 7)
+    echo "Aluno: $nome, Nota: $nota\n";
 }
 
-verificarSenha('1234Mcm2');
+// Q10
+// função para caulcular o circulo passando o parâmetro raio
+function calcularAreaCirculo($raio){ 
+    $pi = 3.14159; // Define a constante Pi com valor aproximado 3.14159
+   $area = $pi * ($raio * $raio); // calcula a area do circulo usando a formula a = pi * r^2
+   return $area; // retorna a área calculada
+    
+}
+
+// Valor teste
+$raio = 4; // define um valor teste para raio
+$area = calcularAreaCirculo($raio); // chama a função calcularAreaCirculo com o valor de teste e armazena o resultado na variavel $area
+echo "A área do círculo com valor teste raio $raio é $area"; // imprime a área do circulo com o valor de teste do raio
+
+// Q11
+function numerosPares($n){
+    $pares = array(); // inicia um array vazio para armazenar números pares
+    for ($a = 0; $a <= $n; $a++){ // faz a iteração de 0 até o número fornecido
+        if($a % 2 == 0){ // verifica se o número é par
+            $pares[] = $a; // adicona o número par ao array esperado
+        }
+    }
+    return $pares; // retorna o array com os números pares até zero
+}
+
+$numero = 10; // 
+$meuArray = numerosPares($numero); // chama a função com o valor do teste passado e armaneza o resultado em $meuArray
+
+// percorre e faz a exibição do array resultante 
+foreach ($meuArray as $numeroPar) { 
+    echo $numeroPar . " ";  // exibe cada número par 
+}
